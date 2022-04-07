47ae6481f47c6b0f2c665b07b418aa5afbf03155
soveetrout <95294452+soveetrout@users.noreply.github.com>
2022-04-07 21:08:05 +0530

    Add files via upload

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,4 +74,28 @@ class HomeController extends Controller
 
         return redirect()->back()->with('message','Appointment sent. We will contact you soon.');
     }
+
+
+    public function myappointment()
+    {
+        if(Auth::id())
+        {
+            $userid=Auth::user()->id;
+            $appoint=appointment::where('user_id',$userid)->get();
+            return view('user.my_appointment',compact('appoint'));
+        }
+        else
+        {
+            return redirect()->back();
+        }
+        
+    }
+
+
+    public function cancel_appoint($id)
+    {
+        $data=appointment::find($id);
+        $data->delete();
+        return redirect()->back();
+    }
 }
